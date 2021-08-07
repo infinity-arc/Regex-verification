4f45ce1699e2f987e425643dc6b1f931ac369577
Kobus <kobus@infinityarc.net>
2021-08-07 11:09:39 +0200

    added JSON file

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-/*
-h1 -> heading/title
-section -> form section
-capForm -> form location
-form -> labels and input
-prodNameVal/venNumVal -> target
-id -> target location
-placeholder -> input required
-value -> input
-pattern -> validation test
-oninvalid -> hint/required format 
-result -> validation
-submit -> finalize input
-script -> javaScript source
-*/
-
 const prodNameVal = document.getElementById("prodName");
 prodNameVal.addEventListener('change', function(event) {
     const value = event.target.value;
