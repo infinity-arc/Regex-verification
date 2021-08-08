fa6a330bf8814d5b4fe4c3f686f41fc96d30071b
Kobus <kobus@infinityarc.net>
2021-08-08 10:59:43 +0200

    cleaned up dead code

--- script.js
+++ script.js
@@ -9,8 +9,3 @@ venNumVal.addEventListener('change', function(event) {
     const value = event.target.value;
     const result = value.match(venReg);
 })
-
-const subBtn = document.getElementById('submit');
-// subBtn.addEventListener('submit', function(event) {
-//     event.preventDefault();
-// })
